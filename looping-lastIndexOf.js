51b44720339bbb22b3030cb66ef0c965f5c3757d
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-01-06 13:39:41 -0700

    add looping-lastIndexOf quiz

---
+++ looping-lastIndexOf.js
@@ -0,0 +1,20 @@
+
+
+const lastIndexOf = function(arr , val){
+
+  let ans = -1;
+  arr.forEach(
+    function(element,index){
+      ans = element === val ? index : ans;
+    }
+  );
+  return ans;
+
+}
+
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
